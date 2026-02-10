d43e4cfdf1d6965eb682456fb9f1719c2eecf046
Connor Peacock <cnrblakefinance2@zohomail.eu>
2026-02-10 12:18:01 +0000

    Added urls.py

--- studentpac/urls.py
+++ studentpac/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from studentpac import pacsystem
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
