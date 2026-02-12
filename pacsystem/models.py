fcdd5cee1b9bf9ba93aaf0f0611fefb403d38935
SkyHawk_0 <joshuakhawkins06@outlook.com>
2026-02-12 11:36:49 +0000

    PAC table implemented (NOT TESTED)
    The PAC table as been implemented with: PAC ID, first name, last name, e-mail and department.

--- pacsystem/models.py
+++ pacsystem/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class PacTable(models.Model):
+    PacID = models.AutoField(primary_key=True)
+    FirstName = models.CharField(max_length=255)
+    LastName = models.CharField(max_length=255)
+    Email = models.EmailField(max_length=255)
+    Department = models.CharField(max_length=255)
+
+    def __str__(self):
+        return (f"PAC ID: {self.PacID}\n",
+                f"First Name: {self.FirstName}\n"
+                f"Last Name: {self.LastName}\n"
+                f"Email: {self.Email}\n",
+                f"Department: {self.Department}")
