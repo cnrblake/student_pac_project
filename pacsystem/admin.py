18685071564be97c7342f586557a9f6a61838312
SkyHawk_0 <joshuakhawkins06@outlook.com>
2026-02-17 12:06:39 +0000

    changes to admin.py

--- pacsystem/admin.py
+++ pacsystem/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from . models import PacTable
-from . models import StudentTable
+from . import models
 
 # Register your models here.
-admin.site.register(PacTable)
-admin.site.register(StudentTable)
+admin.site.register(models.PacTable)
+admin.site.register(models.StudentTable)
