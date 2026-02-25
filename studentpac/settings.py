0b270dbfbab203bdd57b69eef96d96a4d5838103
cnrblake <cnrblakefinance2@zohomail.eu>
2026-02-25 14:55:52 +0000

    set debug to false

--- studentpac/settings.py
+++ studentpac/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mzgp!82q-2flx&f2@2jkeytw^(x&)*2u5$q*c&ba5d^nu1ewft'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['student-pac-project.onrender.com', '127.0.0.1']
 
